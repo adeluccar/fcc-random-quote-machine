660f69392b4a28272f486ba5fed26bd5b1099777
Alberto De Lucca <albertodelucca@gmail.com>
2017-07-11 18:18:27 -0400

    Make HtmlWebpackPlugin generate the index
    Moved files into src (source folder). Installed and configured
    HtmlWebpackPlugin to serve the page to the dist folder. Also established
    a template (src/index.html) that will serve the final dist
    (distribution) index.html. Removed the <script> call to app.js since
    webpack is now injecting that one automatically.
    
    Still need to inject jQuery and Twitter JS (which is why the page
    doesn't work ATM)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    filename: './src/app.js'
+  },
   output: {
-    filename: './dist/app.bundle.js'
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Random Quote Generator',
+      template: './src/index.html'
+    })
+  ]
 }
